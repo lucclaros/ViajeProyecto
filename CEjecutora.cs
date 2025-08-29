d77fb2e8ea5430d08650166732b2c4f2763552b0
lucclaros <135435931+lucclaros@users.noreply.github.com>
2025-08-29 16:53:29 -0300

    Add files via upload

---
+++ CEjecutora.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Viaje
+{
+    public class CEjecutora
+    {
+        public static void Main()
+        {
+            float costoporkm;
+            ushort kmMinimo;
+            // 1
+
+            Console.WriteLine("Ingresar el valor del costo por kilometro : ");
+            costoporkm = float.Parse(Console.ReadLine());
+            Console.WriteLine("Ingresar el kilometraje minimo : ");
+            kmMinimo = ushort.Parse(Console.ReadLine());
+          
+
+            CViaje.SetCostoPorKilometro(costoporkm);
+            CViaje.SetKmMinimo(kmMinimo);
+
+            Console.WriteLine("Costo por kilometro : {0}  Kilometraje minimo : {1} ", costoporkm, kmMinimo);
+
+            // 2
+
+            CViaje Viaje1, ViajeMayorRecorrido;
+            string dom = "";
+            ushort distancia_recorrida;
+            Console.WriteLine("Ingresar dominio : ");
+            dom = Console.ReadLine();
+            ViajeMayorRecorrido = new CViaje();
+
+            if(dom != "Fin")
+            {
+                while(dom != "Fin")
+                {
+                    Console.WriteLine("Ingresar distancia recorrida : ");
+                    distancia_recorrida = ushort.Parse(Console.ReadLine());
+                    Viaje1 = new CViaje(dom, distancia_recorrida);
+                    if(Viaje1.MayorDistancia(ViajeMayorRecorrido) > 0)
+                    {
+                        ViajeMayorRecorrido = Viaje1;
+                    }
+                    Console.WriteLine("Ingresar dominio : (para finalizar, escriba 'Fin')");
+                    dom = Console.ReadLine();
+                }
+                // 3
+                Console.WriteLine("VIAJE CON MAYOR RECORRIDO : ");
+                Console.WriteLine(ViajeMayorRecorrido.DarDatos());
+            } 
+            // 4
+            else
+            {
+                Console.WriteLine("No se ingresaron viajes validos.");
+            }
+
+            Console.Write("Fin del programa. Presione <ENTER> para finalizar");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ CViaje.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Viaje
+{
+    public class CViaje
+    {
+        // 1 
+        private static float costo_por_km;
+        private static ushort km_minimo;
+
+        private string dominio;
+        private ushort distancia_recorrida;
+
+        // 2
+        public static void SetCostoPorKilometro(float num)
+        {
+            CViaje.costo_por_km = num;
+        }
+
+        public float GetCostoPorKilometro()
+        {
+            return CViaje.costo_por_km;
+        }
+
+        // 3 
+        public static void SetKmMinimo(ushort num)
+        {
+            CViaje.km_minimo = num;
+        }
+
+        public ushort GetKmMinimo()
+        {
+            return CViaje.km_minimo;
+        }
+        // 4 
+        public CViaje()
+        {
+            this.dominio = "XX XXX XX";
+        }
+        // 5 
+        public CViaje(string dominio, ushort distanciaRecorrida)
+        {
+            this.dominio = dominio;
+            this.distancia_recorrida = distanciaRecorrida;
+        }
+        // 6
+        public void SetDominio(string dominio)
+        {
+            this.dominio = dominio;
+        }
+        public string GetDominio()
+        {
+            return this.dominio;
+        }
+        // 7     (NO SE SI LO HICE BIEN)
+        public void Kilometraje()
+        {
+            ushort num;
+            Console.WriteLine("La distancia recorrida es de : " + this.distancia_recorrida);
+            Console.WriteLine("Si desea agregar kilometros a la distancia recorrida, escriba 1, caso contrario, escriba 0 : ");
+            if(ushort.Parse(Console.ReadLine()) == 1)
+            {
+                Console.WriteLine("Cuanto desea añadir? : ");
+                num = ushort.Parse(Console.ReadLine());
+                this.distancia_recorrida += num;
+            }
+            return;
+        }
+        // 8 
+        public float DarCosto()
+        {
+            if(this.distancia_recorrida < CViaje.km_minimo)
+            {
+                return (CViaje.costo_por_km * CViaje.km_minimo);
+            }
+            return (CViaje.costo_por_km * this.distancia_recorrida);
+        }
+        // 9
+        public float DarCosto(float peajes)
+        {
+            if (this.distancia_recorrida < CViaje.km_minimo)
+            {
+                return ((CViaje.costo_por_km * (float)CViaje.km_minimo) + peajes);
+            }
+            return ((CViaje.costo_por_km * (float)this.distancia_recorrida) + peajes);
+        }
+        // 10
+        public string DarDatos()
+        {
+            Console.WriteLine("Informacion del viaje : ");
+            string datos = "Dominio : " + this.dominio;
+            datos += " - Distancia recorrida : " + this.distancia_recorrida.ToString();
+            datos += " - Costo por kilometro : " + CViaje.costo_por_km.ToString();
+            datos += " - Costo de viaje(sin peajes) : " + DarCosto().ToString();
+
+            return datos;
+        }
+        // 11
+        public ushort MayorDistancia(CViaje otroViaje)
+        {
+            ushort diferencia_km;
+            diferencia_km = (ushort)(this.distancia_recorrida - otroViaje.distancia_recorrida);
+            return diferencia_km;
+        }
+    }
+}
